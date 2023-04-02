99f3592360d2d4d2cc665af62186b9e7f22aab49
Nazar Mosula <nmosula@gmail.com>
2023-04-02 14:48:18 +0300

    added persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,34 @@ import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filtersReducer } from "./filtersSlice";
 
+import {
+  persistStore, persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'phonebook',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, contactsReducer)
+
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedReducer,
     filters: filtersReducer,
   },
+ middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
